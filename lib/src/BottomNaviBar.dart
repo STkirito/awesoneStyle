4011bb5effb090ad46709823e4ac3373318c44ff
STkirito <diegoliderprogamer@gmail.com>
2021-08-21 00:34:44 -0500

    v0.3.5

--- lib/src/BottomNaviBar.dart
+++ lib/src/BottomNaviBar.dart
@@ -27,17 +27,20 @@ class BottomNaviBar extends StatefulWidget {
 class _BottomNaviBarState extends State<BottomNaviBar> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: CurvedNavigationBar(
-        buttonBackgroundColor: widget.buttonBackgroundColor,
-        backgroundColor: widget.backgroundColor ?? Colors.blueAccent,
-        animationCurve: widget.animationCurve ?? Curves.easeOut,
-        animationDuration:
-            widget.animationDuration ?? const Duration(milliseconds: 600),
-        height: widget.height,
-        index: widget.index,
-        items: widget.items,
+    return GestureDetector(
+      child: Container(
+        child: CurvedNavigationBar(
+          buttonBackgroundColor: widget.buttonBackgroundColor,
+          backgroundColor: widget.backgroundColor ?? Colors.blueAccent,
+          animationCurve: widget.animationCurve ?? Curves.easeOut,
+          animationDuration:
+              widget.animationDuration ?? const Duration(milliseconds: 600),
+          height: widget.height,
+          index: widget.index,
+          items: widget.items,
+        ),
       ),
+      onTap: () => setState(() {}),
     );
   }
 }
