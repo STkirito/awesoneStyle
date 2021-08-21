2b6e7f540d87b4337d8f283f358b3a5db7d60202
STkirito <diegoliderprogamer@gmail.com>
2021-08-21 14:06:34 -0500

    v0.3.16

--- lib/awesonestyle.dart
+++ lib/awesonestyle.dart
@@ -7,5 +7,5 @@ export 'package:awesonestyle/src/fondoBorroso.dart';
 export 'package:awesonestyle/src/input_text_1.dart';
 export 'package:awesonestyle/src/text_url_1.dart';
 export 'package:awesonestyle/src/BottonNaviBar.dart';
-export 'package:awesonestyle/src/bottonBarItem.dart';
+export 'package:awesonestyle/src/BottonBarItem.dart';
 
