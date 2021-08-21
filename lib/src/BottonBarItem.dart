53bed877652d7d4da66b3726e750b6b9aa260207
STkirito <diegoliderprogamer@gmail.com>
2021-08-21 14:01:52 -0500

    v0.3.15

--- lib/src/BottonBarItem.dart
+++ lib/src/BottonBarItem.dart
@@ -18,17 +18,17 @@ class BottonBarItem extends StatefulWidget {
       this.backgroundColor,
       this.iconColor});
 
-
+  String get text()=> widget.text;
+    IconData get icon()=> widget.icon;
   @override
   _BottonBarItemState createState() => _BottonBarItemState();
 }
 
 class _BottonBarItemState extends State<BottonBarItem> {
-
+  
   @override
   Widget build(BuildContext context) {
-    String get text()=> widget.text;
-  IconData get icon()=> widget.icon;
+    
     return Container(
       height: widget.height ?? 40,
       width: widget.weight ?? double.infinity,
