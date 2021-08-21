c49b5a6cbdbed626f6dce62e86a3e623a36af9d1
STkirito <diegoliderprogamer@gmail.com>
2021-08-20 23:31:11 -0500

    v0.3.2

--- lib/src/bottomNaviBarItem.dart
+++ lib/src/bottomNaviBarItem.dart
@@ -5,13 +5,19 @@ class BottomNaviBarItem extends StatelessWidget {
   final double? weight;
   final IconData? icon;
   final String? text;
+  final Color? textcolor;
+  final Color? backgroundColor;
+  final Color? iconColor;
   final double? fontSize;
   BottomNaviBarItem(
       {this.height,
       this.weight,
       required this.icon,
       required this.text,
-      this.fontSize});
+      this.fontSize,
+      this.textcolor,
+      this.backgroundColor,
+      this.iconColor});
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -24,14 +30,21 @@ class BottomNaviBarItem extends StatelessWidget {
             direction: Axis.vertical,
             crossAxisAlignment: WrapCrossAlignment.center,
             children: [
-              Icon(icon),
+              Icon(
+                icon,
+                color: iconColor,
+              ),
               SizedBox(
                 height: 2,
               ),
               Material(
                 child: Text(
                   text ?? ' ',
-                  style: TextStyle(fontSize: fontSize ?? 10),
+                  style: TextStyle(
+                    fontSize: fontSize ?? 10,
+                    color: textcolor,
+                    backgroundColor: backgroundColor,
+                  ),
                 ),
               ),
             ],
