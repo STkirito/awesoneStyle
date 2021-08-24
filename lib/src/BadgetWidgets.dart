55c2d73f1b3a3b77619a8947c45fee369d9fe493
STkirito <diegoliderprogamer@gmail.com>
2021-08-24 01:53:05 -0500

    v0.4.5

--- lib/src/BadgetWidgets.dart
+++ lib/src/BadgetWidgets.dart
@@ -1,19 +1,37 @@
 import 'package:flutter/material.dart';
 
 class BadgeWidgets {
-  Widget badgetBox({
-    bool? isIndicator = false,
-    Color? color = Colors.black,
-    double? size,
-    bool? disabled = false,
-    bool? show = true,
-    required Widget? child,
-  }) =>
-      show == true && isIndicator == true
+  bool? isIndicator;
+  Color? color;
+  double? size;
+  bool? disabled;
+  bool? show;
+  Widget? child;
+  BadgeWidgets({
+    isIndicator = false,
+    color = Colors.black,
+    size,
+    disabled = false,
+    show = true,
+    required child,
+  });
+  Widget badgetBox() => show == true && isIndicator == true
+      ? Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.all(3),
+          margin: EdgeInsets.all(7),
+          decoration: new BoxDecoration(
+            color: disabled == false ? color : color!.withOpacity(0.6),
+          ),
+          constraints: BoxConstraints(
+            minWidth: size ?? (isIndicator == true ? 5 : 18),
+            minHeight: size ?? (isIndicator == true ? 5 : 18),
+          ),
+        )
+      : show == true && child != null
           ? Container(
               alignment: Alignment.center,
               padding: EdgeInsets.all(3),
-              margin: EdgeInsets.all(7),
               decoration: new BoxDecoration(
                 color: disabled == false ? color : color!.withOpacity(0.6),
               ),
@@ -21,35 +39,29 @@ class BadgeWidgets {
                 minWidth: size ?? (isIndicator == true ? 5 : 18),
                 minHeight: size ?? (isIndicator == true ? 5 : 18),
               ),
+              child: child,
             )
-          : show == true && child != null
-              ? Container(
-                  alignment: Alignment.center,
-                  padding: EdgeInsets.all(3),
-                  decoration: new BoxDecoration(
-                    color: disabled == false ? color : color!.withOpacity(0.6),
-                  ),
-                  constraints: BoxConstraints(
-                    minWidth: size ?? (isIndicator == true ? 5 : 18),
-                    minHeight: size ?? (isIndicator == true ? 5 : 18),
-                  ),
-                  child: child,
-                )
-              : Container();
+          : Container();
 
-  Widget badgetCircular({
-    bool? isIndicator = false,
-    Color? color = Colors.black,
-    double? size,
-    bool? disabled = false,
-    bool? show = true,
-    required Widget? child,
-  }) =>
-      show == true && isIndicator == true
+  Widget badgetCircular() => show == true && isIndicator == true
+      ? Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.all(3),
+          margin: EdgeInsets.all(7),
+          decoration: new BoxDecoration(
+            color: disabled == false ? color : color!.withOpacity(0.6),
+            borderRadius: BorderRadius.circular(
+                size ?? (isIndicator == true ? 5 : 20) / 2),
+          ),
+          constraints: BoxConstraints(
+            minWidth: size ?? (isIndicator == true ? 5 : 18),
+            minHeight: size ?? (isIndicator == true ? 5 : 18),
+          ),
+        )
+      : show == true && child != null
           ? Container(
               alignment: Alignment.center,
               padding: EdgeInsets.all(3),
-              margin: EdgeInsets.all(7),
               decoration: new BoxDecoration(
                 color: disabled == false ? color : color!.withOpacity(0.6),
                 borderRadius: BorderRadius.circular(
@@ -59,21 +71,7 @@ class BadgeWidgets {
                 minWidth: size ?? (isIndicator == true ? 5 : 18),
                 minHeight: size ?? (isIndicator == true ? 5 : 18),
               ),
+              child: child,
             )
-          : show == true && child != null
-              ? Container(
-                  alignment: Alignment.center,
-                  padding: EdgeInsets.all(3),
-                  decoration: new BoxDecoration(
-                    color: disabled == false ? color : color!.withOpacity(0.6),
-                    borderRadius: BorderRadius.circular(
-                        size ?? (isIndicator == true ? 5 : 20) / 2),
-                  ),
-                  constraints: BoxConstraints(
-                    minWidth: size ?? (isIndicator == true ? 5 : 18),
-                    minHeight: size ?? (isIndicator == true ? 5 : 18),
-                  ),
-                  child: child,
-                )
-              : Container();
+          : Container();
 }
--- lib/src/BottonTabBarItem.dart
+++ lib/src/BottonTabBarItem.dart
@@ -1,12 +1,11 @@
-import 'package:awesonestyle/src/BadgetWidgets.dart';
 import 'package:flutter/material.dart';
 
 class BottonTabBarItem {
   final String text;
   final IconData icon;
-  final BadgeWidgets? badge;
+  final Widget? badge;
   BottonTabBarItem({required this.text, required this.icon, this.badge});
   String get labels => text;
   IconData get icons => icon;
-  BadgeWidgets? get badges => badge;
+  Widget? get badges => badge;
 }
