b97bbadd9aa6ecb1952b0eb994246bdd2e9fdb9c
STkirito <diegoliderprogamer@gmail.com>
2021-08-24 00:45:35 -0500

    v0.4.3

--- lib/src/BottonTabBarItem.dart
+++ lib/src/BottonTabBarItem.dart
@@ -1,15 +1,12 @@
+import 'package:awesonestyle/src/BadgetWidgets.dart';
 import 'package:flutter/material.dart';
 
 class BottonTabBarItem {
   final String text;
   final IconData icon;
-
-  /*final List<IconData> iconData; */
-  BottonTabBarItem({required this.text, required this.icon});
-  //late List<String?> textLis = textLis..add(this.text);
-  //late List<IconData> iconLis = iconLis..add(this.icon);
-  /* List<String?> get labels => textLis;
-  List<IconData>? get icons => iconLis; */
+  final BadgeWidgets? badge;
+  BottonTabBarItem({required this.text, required this.icon, this.badge});
   String get labels => text;
   IconData get icons => icon;
+  BadgeWidgets? get badges => badge;
 }
