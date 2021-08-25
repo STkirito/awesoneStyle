86bd44c88af91a93d449235b0181c081b5daf9c3
STkirito <diegoliderprogamer@gmail.com>
2021-08-24 20:28:20 -0500

    v0.4.13

--- lib/src/BadgetWidgets.dart
+++ lib/src/BadgetWidgets.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
 
 abstract class BadgetWidgets extends StatelessWidget {
-  late final bool isIndicator;
-  late final Color color;
+  late final bool isIndicator = false;
+  late final Color color = Colors.black;
   late final double? size;
-  late final bool disabled;
-  late final bool show;
+  late final bool disabled = false;
+  late final bool show = true;
   late final Widget child;
 }
 
 class BadgeBox extends BadgetWidgets {
   BadgeBox({
-    isIndicator = false,
-    color = Colors.black,
+    isIndicator,
+    color,
     size,
-    disabled = false,
-    show = true,
+    disabled,
+    show,
     required child,
   });
   Widget _badgetBox() => show == true && isIndicator == true
