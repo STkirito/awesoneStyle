d7722c824407c41987dc5c0b3d446499eaa54706
STkirito <diegoliderprogamer@gmail.com>
2021-08-25 01:10:16 -0500

    v0.4.34

--- lib/src/btn_2.dart
+++ lib/src/btn_2.dart
@@ -38,8 +38,8 @@ class _Btn_2State extends State<Btn_2> {
     return GestureDetector(
       child: Container(
         padding: EdgeInsets.all(widget.padding),
-        height: widget.size,
-        width: double.infinity,
+        //height: widget.size,
+        //width: double.infinity,
         decoration: BoxDecoration(
           color: widget.state == true
               ? widget.firstbackgroundColor
@@ -53,6 +53,8 @@ class _Btn_2State extends State<Btn_2> {
           ),
         ),
         child: Container(
+          //height: widget.size,
+          width: double.infinity,
           padding: EdgeInsets.all(5),
           child: Material(
               borderRadius: BorderRadius.circular(40),
